ba85c086bdb49f397ce71d74f6d1f2111a0da068
birdbird314 <grantstadt@gmail.com>
2018-11-10 21:14:28 +0100

    Load hardcoded provider values

--- src/app/weather-providers-admin/weather-providers-admin.component.ts
+++ src/app/weather-providers-admin/weather-providers-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-weather-providers-admin',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-providers-admin.component.css']
 })
 export class WeatherProvidersAdminComponent implements OnInit {
+  providerForm: FormGroup;
+  currentProvider: string;
+  providers: string[];
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.getCurrentProvider();
+    this.getProviders();
+    this.providerForm = this.formBuilder.group({
+        provider: ['', Validators.required]
+    });
   }
 
+  getCurrentProvider(): void {
+    this.currentProvider = 'Baca';
+  }
+
+  getProviders(): void {
+    this.providers = ['Yahoo', 'OpenWeatherMap', 'Baca'];
+  }
+
+  get form() { return this.providerForm.controls; }
+
+  changeProvider(): void {
+  }
 }
