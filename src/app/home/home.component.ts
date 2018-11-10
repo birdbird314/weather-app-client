fc213f160cef4dc4445e7227b7808f9bd216cd6b
birdbird314 <grantstadt@gmail.com>
2018-11-10 16:46:29 +0100

    Add dropdown for cities

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from '../_models/city';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  cities: City[];
+  city: City;
 
   constructor() { }
 
   ngOnInit() {
+    this.cities = [{countryCode: 'PL', id: 1, name: 'Warsaw'}, {countryCode: 'PL', id: 2, name: 'Cracow'}];
+    this.city = this.cities[0];
   }
 
+  setCity(city: City) {
+    this.city = city;
+  }
 }
