307f1aa8b915ad239e9d06caa00b92a47db3796e
birdbird314 <grantstadt@gmail.com>
2018-11-10 22:09:24 +0100

    Implement basic register view

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../_services/authentication.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  passwordsDoNotMatch = false;
 
   constructor(
     private authService: AuthenticationService,
@@ -21,6 +24,30 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
   }
 
+  get form() { return this.registerForm.controls; }
+
+  passwordsMatch(): boolean {
+    let password = this.form.password.value;
+    let confirmPassword = this.form.confirmPassword.value;
+    return password && (password === confirmPassword);
+  }
+
+  validatePasswords(): void {
+    this.passwordsDoNotMatch = !this.passwordsMatch();
+  }
+  
+  onSubmit(): void {
+    this.submitted = true;
+    this.validatePasswords();
+    if (this.registerForm.invalid || this.passwordsDoNotMatch) {
+      return;
+    }
+  }
 }
