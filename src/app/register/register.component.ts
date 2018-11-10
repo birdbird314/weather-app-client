5c17fda2612ad15b91ec4bfcb72685c58cccc4da
birdbird314 <grantstadt@gmail.com>
2018-11-10 21:33:05 +0100

    Redirect to home when user is logged in

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../_services/authentication.service';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { 
+    if (this.authService.isLoggedIn()) { 
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit() {
   }
