5decd223ae5e0d700dc0332f690ceef49ce47e8f
birdbird314 <grantstadt@gmail.com>
2018-11-10 13:20:48 +0100

    Add login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     routing
   ],
   providers: [],
