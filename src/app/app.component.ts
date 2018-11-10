8d3bbe9efda19da073cdbfd27cc20c60e4471b0c
birdbird314 <grantstadt@gmail.com>
2018-11-10 12:39:39 +0100

    Remove default placeholder page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app-client';
+export class AppComponent implements OnInit {
+  public constructor(private titleService: Title) { }
+
+  title = 'weather-app';
+
+  ngOnInit() {
+    this.titleService.setTitle('Weather');
+  }
 }
